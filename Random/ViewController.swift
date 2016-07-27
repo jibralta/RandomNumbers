3530e898bd36f05dc3f7c136f86d752b10c99045
jibralta <joyumali@gmail.com>
2016-07-27 14:04:01 -0700

    Plain app for guessing number of fingers.

--- Random/ViewController.swift
+++ Random/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var enterNumberField: UITextField!
+    @IBOutlet var outputLabel: UILabel!
+    
+    
+    @IBAction func guessButton(_ sender: AnyObject) {
+        
+        let diceRoll = arc4random_uniform(11)
+        
+        if UInt32(enterNumberField.text!) == diceRoll {
+            outputLabel.text = "You're Right!!"
+        }
+        
+        else {
+            if diceRoll > 1 || diceRoll == 0 {
+            outputLabel.text = "Wrong! I was holding up \(diceRoll) fingers."
+        }
+            else {
+            outputLabel.text = "Wrong! I was holding up \(diceRoll) finger."
+
+            }
+    }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
